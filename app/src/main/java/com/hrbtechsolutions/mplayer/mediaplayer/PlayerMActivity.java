11eedc5e5b76258deebceb3b15e2c59872279997
Hassan Bokhari <y2herc@gmail.com>
2017-06-07 13:23:17 -0400

    Initial commit

---
+++ app/src/main/java/com/hrbtechsolutions/mplayer/mediaplayer/PlayerMActivity.java
@@ -0,0 +1,13 @@
+package com.hrbtechsolutions.mplayer.mediaplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_m);
+    }
+}
