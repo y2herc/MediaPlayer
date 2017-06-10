e96a386e6e0e6665f0f6db9c3359a8db7396b061
Hassan Bokhari <y2herc@gmail.com>
2017-06-10 16:13:18 -0400

    Realtime Time counting using thread implemented. Added seek bar REaltime update and touch to seek capability.

--- app/src/main/java/com/hrbtechsolutions/mplayer/mediaplayer/PlayerMActivity.java
+++ app/src/main/java/com/hrbtechsolutions/mplayer/mediaplayer/PlayerMActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import java.util.concurrent.TimeUnit;
+import android.os.Handler;
 
 public class PlayerMActivity extends AppCompatActivity {
 
@@ -23,8 +25,9 @@ public class PlayerMActivity extends AppCompatActivity {
     TextView AlbumTitle;
     TextView TotalTime;
     TextView CurrentTime;
-
-    long totalTime,currentTime;
+    SeekBar seekBar;
+    Handler handler=new Handler();
+    long totalTime, currentTime;
 
 
     @Override
@@ -32,35 +35,61 @@ public class PlayerMActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_m);
 
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.aitebaar);
+
         Uri uri;
-        uri= Uri.parse("android.resource://com.hrbtechsolutions.mplayer.mediaplayer/"+R.raw.aitebaar);
+        uri = Uri.parse("android.resource://com.hrbtechsolutions.mplayer.mediaplayer/" + R.raw.aitebaar);
 
-        buttonPlay=(Button)findViewById(R.id.PlayButton);
-        buttonStop=(Button)findViewById(R.id.StopButton);
-        buttonPause=(Button)findViewById(R.id.PauseButton);
-        SongTitle=(TextView)findViewById(R.id.TitleOfSong);
-        ArtistTitle=(TextView)findViewById(R.id.ArtistOfSong);
-        AlbumTitle=(TextView)findViewById(R.id.AlbumOfSong);
-        TotalTime=(TextView)findViewById(R.id.TotalTime) ;
-        CurrentTime=(TextView)findViewById(R.id.CurrentTime);
-
-        MediaMetadataRetriever retriever=new MediaMetadataRetriever();
-        retriever.setDataSource(getApplicationContext(),uri);
-        String name=retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
-        ArtistTitle.setText(name);
-        name=retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
-        SongTitle.setText(name);
-        name=retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
-        AlbumTitle.setText(name);
-        mediaPlayer =MediaPlayer.create(getApplicationContext(),R.raw.aitebaar);
+        buttonPlay = (Button) findViewById(R.id.PlayButton);
+        buttonStop = (Button) findViewById(R.id.StopButton);
+        buttonPause = (Button) findViewById(R.id.PauseButton);
+        SongTitle = (TextView) findViewById(R.id.TitleOfSong);
+        ArtistTitle = (TextView) findViewById(R.id.ArtistOfSong);
+        AlbumTitle = (TextView) findViewById(R.id.AlbumOfSong);
+        TotalTime = (TextView) findViewById(R.id.TotalTime);
+        CurrentTime = (TextView) findViewById(R.id.CurrentTime);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        totalTime = mediaPlayer.getDuration();
+        seekBar.setMax((int)totalTime);
 
 
-        totalTime=mediaPlayer.getDuration();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                int i=seekBar.getProgress();
+                mediaPlayer.seekTo(i);
+                mediaPlayer.start();
+            }
+        });
+
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(getApplicationContext(), uri);
+
+        ArtistTitle.setText(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST));
+        SongTitle.setText(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
+        AlbumTitle.setText(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM));
+
+
+
+
+
         TotalTime.setText(String.format("%d:%d",
-                TimeUnit.MILLISECONDS.toMinutes( totalTime),
-                TimeUnit.MILLISECONDS.toSeconds( totalTime))
+                TimeUnit.MILLISECONDS.toMinutes(totalTime),
+                TimeUnit.MILLISECONDS.toSeconds(totalTime))
         );
-
 
 
         buttonPlay.setOnClickListener(new View.OnClickListener() {
@@ -68,8 +97,12 @@ public class PlayerMActivity extends AppCompatActivity {
             public void onClick(View view) {
 
 
-                if(!mediaPlayer.isPlaying())
+                if (!mediaPlayer.isPlaying()) {
+             //       seekBar.setProgress(0);
+
                     mediaPlayer.start();
+                    handler.postDelayed(UpdateSongTime,100);
+                }
             }
 
         });
@@ -77,17 +110,18 @@ public class PlayerMActivity extends AppCompatActivity {
         buttonStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mediaPlayer !=null && mediaPlayer.isPlaying()){
+                if (mediaPlayer != null && mediaPlayer.isPlaying()) {
                     mediaPlayer.seekTo(0);
                     mediaPlayer.pause();
 
+                }
             }
-        }});
+        });
 
         buttonPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mediaPlayer.isPlaying())
+                if (mediaPlayer.isPlaying())
                     mediaPlayer.pause();
 
             }
@@ -95,7 +129,22 @@ public class PlayerMActivity extends AppCompatActivity {
     }
 
 
-
-
-
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            currentTime = mediaPlayer.getCurrentPosition();
+           if(TimeUnit.MILLISECONDS.toSeconds(currentTime)<10&&TimeUnit.MILLISECONDS.toMinutes(currentTime)<1){
+            CurrentTime.setText(String.format("%d:0%d",
+                    TimeUnit.MILLISECONDS.toMinutes( currentTime),
+                    TimeUnit.MILLISECONDS.toSeconds( currentTime) ));
+           }
+            else
+           {
+               CurrentTime.setText(String.format("%d:%d",
+                   TimeUnit.MILLISECONDS.toMinutes( currentTime),
+                   TimeUnit.MILLISECONDS.toSeconds( currentTime) ));
+           }
+            seekBar.setProgress((int)currentTime);
+            handler.postDelayed(this, 100);
+        }
+    };
 }
