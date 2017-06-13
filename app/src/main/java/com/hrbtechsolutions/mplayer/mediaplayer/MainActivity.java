4f22f1fdee87a6661a528d5a107e98b1d874bd35
Hassan Bokhari <y2herc@gmail.com>
2017-06-13 18:39:31 -0400

    STarted working on the main listview activity . customer arrayadapter added. customer listview layout added. viewholder added on the arrayadapter. lstview working with dummy data .app crashing with contentresolver

--- app/src/main/java/com/hrbtechsolutions/mplayer/mediaplayer/MainActivity.java
+++ app/src/main/java/com/hrbtechsolutions/mplayer/mediaplayer/MainActivity.java
@@ -1,14 +1,22 @@
 package com.hrbtechsolutions.mplayer.mediaplayer;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -18,23 +26,19 @@ public class MainActivity extends AppCompatActivity {
     private final String track_name = MediaStore.Audio.Media.TITLE;
     private final String artist = MediaStore.Audio.Media.ARTIST;
 
-
+    MediaPlayer mediaPlayer=new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-/*        arrayofSongs.add(new Song("Hasssan","New"));
-        arrayofSongs.add(new Song("Hasssan","New"));
-        arrayofSongs.add(new Song("Hasssan","New"));
-        arrayofSongs.add(new Song("Hasssan","New"));
-        arrayofSongs.add(new Song("Hasssan","New"));        arrayofSongs.add(new Song("Hasssan","New"));
-        arrayofSongs.add(new Song("Hasssan","New"));
-        arrayofSongs.add(new Song("Hasssan","New"));
-        arrayofSongs.add(new Song("Hasssan","New"));
-        arrayofSongs.add(new Song("Hasssan","New"));*/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
 
-
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+                return;
+            }}
 
         ContentResolver contentResolver = getContentResolver();
         Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
@@ -45,21 +49,48 @@ public class MainActivity extends AppCompatActivity {
         } else if (!cursor.moveToFirst()) {
             // no media on the device
         } else {
-            int titleColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+            int titleColumn = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             int artistColumn = cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            int dataColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DATA);
             do {
                 String thisartist = cursor.getString(artistColumn);
                 String thisTitle = cursor.getString(titleColumn);
+                String thisdata=cursor.getString(dataColumn);
 
-                arrayofSongs.add(new Song(thisTitle,thisartist));
+                arrayofSongs.add(new Song(thisTitle,thisartist,thisdata));
 
             } while (cursor.moveToNext());
         }
+       final MediaPlayer mediaPlayer=new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
         SongAdapter adapter=new SongAdapter(this,arrayofSongs);
+        AdapterView.OnItemClickListener mMessageClickedHandler = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // Do something in response to the click
+
+                Uri myUri=Uri.parse(arrayofSongs.get(position).dataPath);
+
+                try {
+
+                    mediaPlayer.setDataSource(getApplicationContext(), myUri);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                Toast.makeText(getApplicationContext(), "Your toast message."+position,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        };
+
 
         ListView listView=(ListView)findViewById(R.id.listofsongs);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(mMessageClickedHandler);
     }
 
 
--- app/src/main/java/com/hrbtechsolutions/mplayer/mediaplayer/Song.java
+++ app/src/main/java/com/hrbtechsolutions/mplayer/mediaplayer/Song.java
@@ -8,11 +8,13 @@ public class Song {
 
     public String songName;
     public String artistName;
-    public Song(String song, String artist){
+    public String dataPath;
+
+    public Song(String song, String artist,String datapath){
 
         this.songName=song;
         this.artistName=artist;
-
+        this.dataPath=datapath;
     }
 
 }
